ad1ac2a2cda836f4f5941f9e64a29e98aa933ed8
이정환 <king199777@gmail.com>
2021-07-22 13:45:38 +0900

    [INIT] : #4  Material UI 적용완료

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,11 @@
-import React, { useState } from "react";
-import Head from "next/head";
+import React from "react";
 import Test from "components/test";
 
 export default function Home() {
-    const [text, setText] = useState<string>("자바스크립트");
-    const [title, setTitle] = useState<number>("하이");
-    setTimeout(() => {
-        setText(0);
-        // setText("타입스크립트")
-    }, 1000);
-
     return (
         <div className="container">
             <div>
-                <span>{text} 적용 완료</span>
                 <Test />
-                {title}
             </div>
         </div>
     );
