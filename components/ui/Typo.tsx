47a3550119d3404770ddc3315e5ac01cf57c27fc
김효빈 <52522662+hbin12212@users.noreply.github.com>
2021-07-23 17:07:59 +0900

    [STYLE] : 타이포시스템 정의 완료

---
+++ components/ui/Typo.tsx
@@ -0,0 +1,34 @@
+import styles from "./font.module.scss";
+
+type Props = {
+    type: "BUTTON" | "HEADER" | "TEXT";
+    size: "huge" | "large" | "medium" | "small" | "h1" | "h2" | "h3";
+    color:
+        | "brown_base"
+        | "brown_font"
+        | "mint_accent"
+        | "yellow_accent"
+        | "red_accent"
+        | "gray_accent"
+        | "light_accent"
+        | "white";
+    content: string;
+};
+
+const Typo = ({ type, size, content, color }: Props) => {
+    switch (type) {
+        case "BUTTON":
+            return <span className={`${styles[size]} ${styles[color]}`}>{content}</span>;
+        case "HEADER":
+            var headerSize = size + "_header";
+            return <p className={`${styles[headerSize]} ${styles[color]}`}>{content}</p>;
+        case "TEXT":
+            var textSize = size + "_text";
+            return <p className={`${styles[textSize]} ${styles[color]}`}>{content}</p>;
+        case "TEXT":
+        default:
+            return <div></div>;
+    }
+};
+
+export default Typo;
