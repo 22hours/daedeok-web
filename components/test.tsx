524f3c49a4e8797920a150578b37fd800afb1b2b
김효빈 <52522662+hbin12212@users.noreply.github.com>
2021-07-23 14:21:52 +0900

    [STYLE] : #7 폰트 타이포시스템 적용 완료

--- components/test.tsx
+++ components/test.tsx
@@ -3,7 +3,7 @@ import styles from "./ui/font.module.scss";
 type Props = {};
 
 const test = () => {
-    return <div>d</div>;
+    return <div className={styles.regular_h4}>text</div>;
 };
 
 export default test;
