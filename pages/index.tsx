e21ed9e2832dcecbbc46b55ce85b7c6ca00d7f98
이정환 <king199777@gmail.com>
2021-07-27 15:25:39 +0900

    [FIX] : Remove unsuse component imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,12 @@
 import GlobalLayout from "components/layout/GlobalLayout";
 
 import React from "react";
-import Test from "components/test";
 
 // HOME
 export default function Home() {
     return (
         <div className="container">
-            <div>
-                <Test />
-            </div>
+            <div></div>
         </div>
     );
 }
