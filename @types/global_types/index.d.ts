370a7c16da7bb2bc0dd53df7d34f63c77a702c02
이정환 <king199777@gmail.com>
2021-07-28 12:51:21 +0900

    [FEAT] : #33 타입명세모듈 INIT

---
+++ @types/global_types/index.d.ts
@@ -0,0 +1,16 @@
+//  ./@types/custom-types/index.d.ts
+declare module "@global_types" {
+    export namespace meta_types {
+        type user = {
+            user_id: number;
+            name: string;
+            role: "ROLE_MEMBER";
+            duty: string;
+            lecture_num: 3;
+            access_token: string;
+            refresh_token: string;
+        };
+    }
+    export namespace req_types {}
+    export namespace res_types {}
+}
