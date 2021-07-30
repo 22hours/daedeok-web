92eefc33617e501b8351e6c76cbec9d28610afa8
이정환 <king199777@gmail.com>
2021-07-30 13:34:14 +0900

    [FEAT] : Typo 수정

--- components/ui/Typo.tsx
+++ components/ui/Typo.tsx
@@ -1,9 +1,8 @@
 import styles from "./Typo.module.scss";
 
-type Props = {
-    type: "BUTTON" | "HEADER" | "TEXT";
-    size: "huge" | "large" | "medium" | "small" | "smaller" | "h1" | "h2" | "h3";
-    color:
+type CommonProps = {
+    content: string;
+    color?:
         | "brown_base"
         | "brown_font"
         | "mint_accent"
@@ -12,19 +11,49 @@ type Props = {
         | "gray_accent"
         | "light_accent"
         | "white";
-    content: string;
+    className?: string;
 };
 
-const Typo = ({ type, size, content, color }: Props) => {
+type ButtonProps = {
+    type: "BUTTON";
+    size: "huge" | "large" | "medium" | "small" | "smaller";
+};
+
+type HeaderProps = {
+    type: "HEADER";
+    size: "h1" | "h2" | "h3";
+};
+
+type TextProps = {
+    type: "TEXT";
+    size: "huge" | "large" | "medium" | "small" | "smaller";
+};
+
+type Props = (CommonProps & ButtonProps) | (CommonProps & HeaderProps) | (CommonProps & TextProps);
+
+const Typo = ({ type, size, content, color, className }: Props) => {
+    const styleColor = color || "brown_base";
     switch (type) {
         case "BUTTON":
-            return <span className={`${styles[size]} ${styles[color]}`}>{content}</span>;
+            const buttonStyle = `${className || ""} ${styles[size]} ${styles[styleColor]}`;
+            return <span className={buttonStyle}>{content}</span>;
         case "HEADER":
             var headerSize = size + "_header";
-            return <p className={`${styles[headerSize]} ${styles[color]}`}>{content}</p>;
+            const headerStyle = `${className || ""} ${styles[headerSize]} ${styles[styleColor]}`;
+            switch (size) {
+                case "h1":
+                    return <h1 className={headerStyle}>{content}</h1>;
+                case "h2":
+                    return <h2 className={headerStyle}>{content}</h2>;
+                case "h3":
+                    return <h3 className={headerStyle}>{content}</h3>;
+                default:
+                    return <h5>{content}</h5>;
+            }
         case "TEXT":
             var textSize = size + "_text";
-            return <p className={`${styles[textSize]} ${styles[color]}`}>{content}</p>;
+            const textStyle = `${className || ""} ${styles[textSize]} ${styles[styleColor]}`;
+            return <span className={textStyle}>{content}</span>;
         default:
             return <span></span>;
     }
