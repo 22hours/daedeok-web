59b3f0896b136c8acdab210263e28d8d61e6ee31
이정환 <king199777@gmail.com>
2021-08-02 14:10:42 +0900

    [FEAT] : #38 Firebase phone auth 적용및 테스트완료

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,15 +1,21 @@
 import React, { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
 import useAgent from "lib/hooks/useAgent";
+
 type Props = {};
 
 const GlobalLayout = dynamic(import("components/layout/GlobalLayout"));
 
+const RegisterTest = dynamic(import("components/organism/RegisterTest"));
+
 const Register = () => {
     const agent = useAgent();
+
     return (
         <GlobalLayout isBannerHide={true} isMenuHide={agent === "mobile"}>
             <h2>register</h2>
+
+            <RegisterTest />
         </GlobalLayout>
     );
 };
