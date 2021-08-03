8a2c9372e1627980820426d241a509262cac994d
이정환 <king199777@gmail.com>
2021-08-03 20:35:57 +0900

    [FEAT] : #64

--- pages/class/[status]/[class_id]/manage/attendance/index.tsx
+++ pages/class/[status]/[class_id]/manage/attendance/index.tsx
@@ -1,6 +1,20 @@
+import { useEffect } from "react";
+import { useAuthStore } from "store/AuthStore";
+import { useClassStore } from "store/ClassStore";
+
 type Props = {};
 
-const index = () => {
+const Index = () => {
+    const classState = useClassStore();
+    useEffect(() => {
+        classState.dispatch({
+            type: "SET_BREADCRUMBS_LIST",
+            data: [
+                { name: "강의실 입장", link: "/class" },
+                { name: "강의실 입장", link: "/class" },
+            ],
+        });
+    }, []);
     return (
         <div>
             <h2>index</h2>
@@ -8,4 +22,4 @@ const index = () => {
     );
 };
 
-export default index;
+export default Index;
