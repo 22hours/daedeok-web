23da4d9ea0f682419a23115b0da29d88a51257d1
이정환 <king199777@gmail.com>
2021-08-09 16:21:47 +0900

    [FEAT] : #123

--- pages/class/notice/edit/[...notice_id].tsx
+++ pages/class/notice/edit/[...notice_id].tsx
@@ -1,16 +1,47 @@
 import React, { useState, useEffect } from "react";
+import TutorNoticeEditor from "components/organism/TutorNoticeEditor/TutorNoticeEditor";
+import { useAuthStore } from "store/AuthStore";
+import { useRouter } from "next/router";
+
 type Props = {};
 
+type State = {
+    title: string;
+    content: string;
+};
 const NoticeEdit = () => {
-    return (
-        <div>
-            <h2>[...article_id]</h2>
-        </div>
-    );
+    const router = useRouter();
+    const { notice_id } = router.query;
+    const { clientSideApi } = useAuthStore();
+    const [originData, setOriginData] = useState<State | null>(null);
+
+    const getOriginData = async () => {
+        const res = await clientSideApi("GET", "MAIN", "TUTOR_NOTICE_FIND_DETAIL", { notice_id: notice_id });
+        if (res.result === "SUCCESS") {
+            setOriginData({
+                title: res.data.title,
+                content: res.data.content,
+            });
+        } else {
+            alert(res.msg);
+        }
+    };
+
+    useEffect(() => {
+        getOriginData();
+    }, []);
+    if (!originData) {
+        return <div>NOW LOADING</div>;
+    } else {
+        return (
+            <div>
+                <TutorNoticeEditor type={"EDIT"} originData={originData} />
+            </div>
+        );
+    }
 };
 
 export async function getServerSideProps(ctx) {
-    console.log(ctx);
     return {
         props: {},
     };
