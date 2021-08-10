0c2e4cc97f475678be7d8ffb172c1af97f8c8bcf
김효빈 <52522662+hbin12212@users.noreply.github.com>
2021-08-10 13:57:26 +0900

    [FIX] : toast-ui 에디터 수정

--- components/molecule/TextEditor/TextEditor.tsx
+++ components/molecule/TextEditor/TextEditor.tsx
@@ -1,6 +1,6 @@
 import style from "./TextEditor.module.scss";
+import "@toast-ui/editor/dist/toastui-editor.css";
 import { Editor, Viewer } from "@toast-ui/react-editor";
-import "@toast-ui/react-editor/dist/toastui-editor.css";
 import { useAuthStore } from "store/AuthStore";
 import { RefObject } from "react";
 
