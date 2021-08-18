6b074af5b8df03fbcbcd43bde431d396ea4e3a59
이정환 <king199777@gmail.com>
2021-08-18 20:39:37 +0900

    [FIX] : 날짜, 시간 입력(강의개설)

--- components/ui/input/TextInput.tsx
+++ components/ui/input/TextInput.tsx
@@ -1,4 +1,5 @@
 import useFormStyle from "lib/hooks/useFormStyle";
+import { useRef, useState } from "react";
 
 type Props = {
     refs?: HTMLInputElement | null;
@@ -32,20 +33,34 @@ const getInputType = (type: Props["type"]) => {
 };
 
 const TextInput = (props: Props) => {
+    const [isFocus, setIsFocus] = useState<boolean>(false);
     const classNames = useFormStyle({ ...props, type: "basic" });
-
     return (
         <div className={Object.values(classNames).concat(props.className).join(" ")}>
-            <input
-                //@ts-ignore
-                ref={props.refs}
-                type={getInputType(props.type)}
-                value={props.value}
-                onChange={props.onChange}
-                maxLength={props.maxLength || undefined}
-                placeholder={props.placeholder || undefined}
-                disabled={props.disable}
-            />
+            {["date", "time"].includes(props.type) ? (
+                <input
+                    //@ts-ignore
+                    ref={props.refs}
+                    type={!isFocus ? "text" : getInputType(props.type)}
+                    value={props.value}
+                    onChange={props.onChange}
+                    maxLength={props.maxLength || undefined}
+                    placeholder={props.placeholder || undefined}
+                    disabled={props.disable}
+                    onFocus={() => setIsFocus(true)}
+                />
+            ) : (
+                <input
+                    //@ts-ignore
+                    ref={props.refs}
+                    type={getInputType(props.type)}
+                    value={props.value}
+                    onChange={props.onChange}
+                    maxLength={props.maxLength || undefined}
+                    placeholder={props.placeholder || undefined}
+                    disabled={props.disable}
+                />
+            )}
         </div>
     );
 };
