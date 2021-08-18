3989237ec71531e8ab504855c0c594730bff6be1
김효빈 <52522662+hbin12212@users.noreply.github.com>
2021-08-18 14:22:33 +0900

    [FIX] : 사이드바 사이즈 수정

--- components/organism/ClassSidebar/ClassSideBar.tsx
+++ components/organism/ClassSidebar/ClassSideBar.tsx
@@ -158,7 +158,7 @@ const ClassSideBar = () => {
     const [mode, setMode] = useState<"pc" | "mobile">("pc");
 
     const setModeByWindowSize = () => {
-        if (window.innerWidth > 450) {
+        if (window.innerWidth > 775) {
             setMode("pc");
         } else {
             setMode("mobile");
