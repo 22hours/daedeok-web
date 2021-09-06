49766c7dbe5bdd4f70c0f4015b806f73e7624a11
김효빈 <52522662+hbin12212@users.noreply.github.com>
2021-09-06 13:48:50 +0900

    [FIX] : 에디터 이미지 수정

--- components/molecule/TextViewer/TextViewer.tsx
+++ components/molecule/TextViewer/TextViewer.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useState } from "react";
 import WindowController from "lib/client/windowController";
 import "react-quill/dist/quill.snow.css";
 
@@ -7,11 +7,36 @@ type Props = {
 };
 
 const TextViewer = (props: Props) => {
-    var width = WindowController.getWindowSize();
+    const [mode, setMode] = useState<"pc" | "mobile">("pc");
+
+    const setModeByWindowSize = () => {
+        var width = WindowController.getWindowSize();
+        console.log(width);
+        if (width > 420) {
+            setMode("pc");
+        } else {
+            setMode("mobile");
+        }
+    };
+
+    useEffect(() => {
+        setModeByWindowSize();
+        window.addEventListener("resize", setModeByWindowSize);
+        return () => {
+            window.removeEventListener("resize", setModeByWindowSize);
+        };
+    }, []);
+
     return (
         <div className={`quill`}>
             <div className={"ql-container"}>
-                <div className={"ql-editor"} dangerouslySetInnerHTML={{ __html: props.content }}></div>
+                {mode === "pc" ? (
+                    <div className={"ql-editor"} dangerouslySetInnerHTML={{ __html: props.content }}></div>
+                ) : (
+                    <div className="ql-mobile-editor">
+                        <div className={"ql-editor"} dangerouslySetInnerHTML={{ __html: props.content }}></div>
+                    </div>
+                )}
             </div>
         </div>
     );
